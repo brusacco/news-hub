556edcf9f31aed5845dd93df4afa08085ebf9240
Bruno Sacco <brusacco@gmail.com>
2025-04-15 23:17:47 -0300

    Update production configuration: disable full error reports and enable static file serving

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local = true
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
   # Ensures that a master key has been made available in ENV["RAILS_MASTER_KEY"], config/master.key, or an environment
@@ -24,7 +24,7 @@ Rails.application.configure do
   # config.require_master_key = true
 
   # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
-  # config.public_file_server.enabled = false
+  config.public_file_server.enabled = false
 
   # Compress CSS using a preprocessor.
   config.assets.css_compressor = nil
