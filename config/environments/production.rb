ef6011eca5253c5e3b9914991745fc1f8da65dfc
Bruno Sacco <brusacco@gmail.com>
2025-04-15 23:59:54 -0300

    aaa

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,25 @@ Rails.application.configure do
   # config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = false
+  config.force_ssl = true
+
+  config.action_dispatch.trusted_proxies = [
+    IPAddr.new('173.245.48.0/20'),
+    IPAddr.new('103.21.244.0/22'),
+    IPAddr.new('103.22.200.0/22'),
+    IPAddr.new('103.31.4.0/22'),
+    IPAddr.new('141.101.64.0/18'),
+    IPAddr.new('108.162.192.0/18'),
+    IPAddr.new('190.93.240.0/20'),
+    IPAddr.new('188.114.96.0/20'),
+    IPAddr.new('197.234.240.0/22'),
+    IPAddr.new('198.41.128.0/17'),
+    IPAddr.new('162.158.0.0/15'),
+    IPAddr.new('104.16.0.0/13'),
+    IPAddr.new('104.24.0.0/14'),
+    IPAddr.new('172.64.0.0/13'),
+    IPAddr.new('131.0.72.0/22')
+  ]
 
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new($stdout)
