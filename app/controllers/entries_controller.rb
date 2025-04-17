9154483f49f68fce2a768463829d44fca0632f36
Bruno Sacco <brusacco@gmail.com>
2025-04-17 20:10:54 -0300

    Feat: Add meta tags for SEO in entry show action

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -7,5 +7,18 @@ class EntriesController < ApplicationController
     @entry = Entry.friendly.find(params[:id])
     @entries = Entry.tagged_with(@entry.tags, any: true).order(published_at: :desc).limit(9)
     @entries = Entry.where.not(id: @entry.id).order(published_at: :desc).limit(9) unless @entries.any?
+
+    set_meta_tags title: @entry.title,
+                  description: @entry.description,
+                  keywords: 'Nintendo, News, Aggregator, Switch, Wii U',
+                  og: {
+                    title: :title,
+                    description: :description,
+                    site_name: 'NintendoNewsHub.com',
+                    type: 'website',
+                    url: entry_url(@entry),
+                    image: @entry.image_url
+                  },
+                  twitter: { card: 'summary' }
   end
 end
