0fc29a4a104aeb281ebc8bc5b500896b0918ef59
Bruno Sacco <brusacco@gmail.com>
2026-06-22 16:56:22 -0300

    Refactor sitemap configuration: update priority and change frequency for entries, games, genres, developers, and tags; add last modified logic for tags

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,7 +1,14 @@
+# frozen_string_literal: true
+
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = 'https://www.nintendonewshub.com'
 
 SitemapGenerator::Sitemap.create do
+  tag_lastmod = lambda do |tag|
+    tagged_entries_updated_at = Entry.tagged_with(tag.name, on: :tags).maximum(:updated_at)
+    [tag.updated_at, tagged_entries_updated_at].compact.max
+  end
+
   # Put links creation logic here.
   #
   # The root path '/' and sitemap index file are added automatically for you.
@@ -15,30 +22,31 @@ SitemapGenerator::Sitemap.create do
   #
   # Examples:
   #
+  add entries_path, priority: 0.9, changefreq: 'hourly'
   add games_path, priority: 0.8, changefreq: 'daily'
-  add genres_path, priority: 0.7, changefreq: 'daily'
-  add developers_path, priority: 0.7, changefreq: 'daily'
-  add tags_path, priority: 0.7, changefreq: 'daily'
+  add genres_path, priority: 0.7, changefreq: 'weekly'
+  add developers_path, priority: 0.7, changefreq: 'weekly'
+  add tags_path, priority: 0.75, changefreq: 'daily'
 
   Game.find_each do |game|
-    add game_path(game), lastmod: game.updated_at, priority: 0.8, changefreq: 'weekly'
+    add game_path(game), lastmod: game.updated_at, priority: 0.75, changefreq: 'weekly'
   end
 
   Genre.find_each do |genre|
-    add genre_path(genre), lastmod: genre.updated_at, priority: 0.7, changefreq: 'weekly'
+    add genre_path(genre), lastmod: genre.updated_at, priority: 0.65, changefreq: 'weekly'
   end
 
   Developer.find_each do |developer|
-    add developer_path(developer), lastmod: developer.updated_at, priority: 0.7, changefreq: 'weekly'
+    add developer_path(developer), lastmod: developer.updated_at, priority: 0.65, changefreq: 'weekly'
   end
 
   Tag.find_each do |tag|
-    add tag_path(tag), lastmod: Time.zone.now, changefreq: 'daily', priority: 0.8
+    add tag_path(tag), lastmod: tag_lastmod.call(tag), changefreq: 'daily', priority: 0.7
   end
   #
   # Add all articles:
   #
   Entry.find_each do |entry|
-    add entry_path(entry), lastmod: entry.updated_at, priority: 1.0, changefreq: 'daily'
+    add entry_path(entry), lastmod: entry.updated_at, priority: 0.95, changefreq: 'daily'
   end
 end
